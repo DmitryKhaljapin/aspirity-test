3473f342107512e9f431fca279b5de847c277587
DmitriX <dodge.241294@gmail.com>
2024-07-01 09:47:41 +0300

    add: colors config was added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,27 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': '#E1E3E6',
+      'primary-bg': '#141414',
+      'primary-br': '#363738',
+
+      'secondery': '#B0B1B6',
+      'secondery-bg': '#232324',
+
+      'tertiary': '#76787A',
+      'tertiary-bg': '#202021',
+
+      'blue': '#0047BB',
+      'dark-blue': '#001A72',
+      'orange': '#FA4616',
+      'white': '#FFFFFF',
+      'black': '#101820', 
+
+      'accent': '#0047BB',
+      'success': '#2E7D32',
+      'error': '#D32F2F',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

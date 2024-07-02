cd5938cba164a2d8a5275312fdb52bcc65ff4d05
DmitriX <dodge.241294@gmail.com>
2024-07-02 13:33:31 +0300

    add: dummy data of team was added

---
+++ dummy_data/team_data.ts
@@ -0,0 +1,58 @@
+import { IEmployeeShort } from '@/../interfaces/employee';
+
+export const teamData: IEmployeeShort[] = [
+    {
+        name: 'John',
+        surname: 'Smith',
+        position: 'Junior UI/UX designer',
+        photoSrc: '/img/John-Smith.png'
+    },
+    {
+        name: 'Sarah',
+        surname: 'Brown',
+        position: 'Middle Product Design TeamLead',
+        photoSrc: '/img/Sarah-Brown.png'
+    },
+    {
+        name: 'David',
+        surname: 'Taylor',
+        position: 'Middle Android Developer',
+        photoSrc: '/img/David-Taylor.png'
+    },
+    {
+        name: 'Ann',
+        surname: 'Williams',
+        position: 'Middle Head of Service',
+        photoSrc: '/img/Ann-Williams.png'
+    }, 
+    {
+        name: 'James',
+        surname: 'Miller',
+        position: 'CEO',
+        photoSrc: '/img/James-Miller.png'
+    },
+    {
+        name: 'Henry',
+        surname: 'Jones',
+        position: 'Middle QA',
+        photoSrc: '/img/Henry-Jones.png'
+    }, 
+    {
+        name: 'Charles',
+        surname: 'Williams',
+        position: 'Junior Employee Support Manager',
+        photoSrc: '/img/Charles-Williams.png'
+    }, 
+    {
+        name: 'Jane',
+        surname: 'Miller',
+        position: 'Middle Sales Manager',
+        photoSrc: '/img/Jane-Miller.png'
+    },
+    {
+        name: 'Sam',
+        surname: 'Clark',
+        position: 'QA Lead',
+        photoSrc: '/img/Sam-Clark.png'
+    }
+]

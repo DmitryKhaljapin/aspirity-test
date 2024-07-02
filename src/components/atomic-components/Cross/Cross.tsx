ded1c3dde622cdfd2df0c649f65e8225c4f01192
DmitriX <dodge.241294@gmail.com>
2024-07-02 09:20:19 +0300

    fix: icon of cross was changed to svg

--- src/components/atomic-components/Cross/Cross.tsx
+++ src/components/atomic-components/Cross/Cross.tsx
@@ -1,9 +1,12 @@
 import { CrossProps } from './Cross.props';
 import styles from './Cross.module.css';
+import Image from 'next/image';
 import cn from 'classnames';
 
 export const Cross = ({className, ...props}: CrossProps): JSX.Element => {
     return (
-        <div className={cn(className, styles.cross)}></div>
+        <div className={cn(className, styles.cross)}>
+            <Image src='/img/icons/cross.svg' width={14} height={14} alt='close' />
+        </div>
     );
 }

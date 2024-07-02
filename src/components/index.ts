95aa1a7d6a9e642fa8b8313f5063a10bdb041830
DmitriX <dodge.241294@gmail.com>
2024-07-02 14:14:40 +0300

    add: reexports was added to components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './Logo/Logo';
+export * from './PersonalInfo/PersonalInfo';
+export * from './ProfileCard/ProfileCard';
+export * from './ProjectInfo/ProjectInfo';
+export * from './TeamList/TeamList';
+export * from './UserTab/UserTab';

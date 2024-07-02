658019ce008ff291d776bc0df9b7e79a2befffcd
DmitriX <dodge.241294@gmail.com>
2024-07-02 09:15:57 +0300

    fix: icons of arrow were changed to svg

--- src/components/atomic-components/Arrow/Arrow.tsx
+++ src/components/atomic-components/Arrow/Arrow.tsx
@@ -1,12 +1,13 @@
 import { ArrowProps } from './Arrow.props';
 import styles from './Arrow.module.css';
+import Image from 'next/image';
 import cn from 'classnames';
 
 export const Arrow = ({className, direction}: ArrowProps): JSX.Element => {
     return (
-        <div className={cn(className, {
-            [styles.down]: direction === 'down',
-            [styles.left]: direction === 'left'
-        })}></div>
+        <div className={cn(className, styles.arrow)}>
+            {direction === 'down' && <Image src='/img/icons/arrow-down.svg' width={14} height={8} alt='' />}
+            {direction === 'left' && <Image src='/img/icons/arrow-left.svg' width={8} height={14} alt='<' />}
+        </div>
     );
 }
